a25d55e615724a318e947cc1d1296f3300df9d83
fran <francisconoppenau@gmail.cm>
2025-11-15 09:21:22 +0100

    uge10opg4

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter 3 digit int:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"the sum of the digits in {num} is {GetSum(num)} "); 
+static int GetSum(int num)
+{
+    int sum = 0;
+    
+    for (int i = num; i > 0; i /= 10)
+    {
+        sum += i % 10;
+    }
+    return sum;
+}
